2bf1856447911da66c95794980a49b15df2c7d7f
Gaston Roxas <gacaro51597@gmail.com>
2022-02-16 14:51:36 +0100

    added design

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: 'Time Raue', address: 'Rudi Duschke Straße', phone_number: '493543523', category: 'japanese')
+Restaurant.create(name: 'Teras Kebab', address: 'Charlottenburgstraße', phone_number: '492141313', category: 'belgian')
+Restaurant.create(name: 'Delhi 66', address: 'Kreuzberg', phone_number: '4998789789', category: 'chinese')
+Restaurant.create(name: 'Rattatouille', address: 'Paris', phone_number: '499304583409', category: 'french')
+Restaurant.create(name: 'Pizza House', address: 'Bundestag', phone_number: '4948359058490', category: 'italian')
